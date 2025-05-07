6ba06df65602e2a6e95ea63e52e727082e632981
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-05-07 23:42:36 +0530

    Github-Auth

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/home.jsx'
+import Dashboard from './components/dashboard.jsx'
+import PrivateRouter from './components/privateRouter.jsx'
+
+function App() {
+  return (
+    <div>
+      <>
+      <Routes>
+        <Route path='/' element={ <Home />} />
+
+        <Route path='/dashboard' element={
+          <PrivateRouter>
+            <Dashboard />
+          </PrivateRouter>
+        } />
+
+      </Routes>
+
+      </>
+    </div>
+  )
+}
+
+export default App
